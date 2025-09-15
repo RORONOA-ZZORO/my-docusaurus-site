752360670bd4421cbde7a2153f941986aa46a49f
pwf-shiwansh <shiwanshbind1@gmail.com>
2025-09-15 23:38:57 +0530

    Fixed and added mermaid also some docs for all the subjects psy, c, es

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,11 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  themes: ['@docusaurus/theme-mermaid'],
+markdown: {
+  mermaid: true,
+},
+
 
   presets: [
     [
@@ -91,7 +96,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
